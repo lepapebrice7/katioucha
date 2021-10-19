8855a459aec50f9a3bf128feca72a803f922b595
lepapeb <lepapebrice7@gmail.com>
2021-10-19 23:25:43 +0200

    feat: adding a log wrapper in utils

---
+++ katioucha/utils/__init__.py
@@ -0,0 +1,14 @@
+import time
+import logging
+from functools import wraps
+
+def logwrap(func):
+    @wraps(func)
+    def with_logging(*args, **kwargs):
+        logging.info(f"Start {func.__name__}")
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        t1 = time.time()
+        logging.info(f"Escaping {func.__name__}. Elapsed time = {t1-t0}s")
+        return result
+    return with_logging

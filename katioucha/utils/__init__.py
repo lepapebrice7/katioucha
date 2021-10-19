6846e491e13367c3d8ce21d90271bfed6b73ba93
lepapeb <lepapebrice7@gmail.com>
2021-10-19 23:50:41 +0200

    feat: adding version and stuff

--- katioucha/utils/__init__.py
+++ katioucha/utils/__init__.py
@@ -2,6 +2,7 @@ import time
 import logging
 from functools import wraps
 
+
 def logwrap(func):
     @wraps(func)
     def with_logging(*args, **kwargs):
@@ -11,4 +12,5 @@ def logwrap(func):
         t1 = time.time()
         logging.info(f"Escaping {func.__name__}. Elapsed time = {t1-t0}s")
         return result
+
     return with_logging
